9eb85178af97833120a05cd245265de2e64a5f89
diepz <you@example.cm>
2018-12-17 11:49:02 +0700

    done angulaer

--- src/app/image-gallery/image-gallery.component.ts
+++ src/app/image-gallery/image-gallery.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, Inject, Optional} from '@angular/core';
+import {GalleryConfig} from './token';
 @Component({
-  selector: 'app-image-gallery',
-  templateUrl: './image-gallery.component.html',
-  styleUrls: ['./image-gallery.component.scss']
+    selector: 'app-image-gallery',
+    templateUrl: './image-gallery.component.html',
+    styleUrls: ['./image-gallery.component.scss']
 })
 export class ImageGalleryComponent implements OnInit {
-  listImage = [
-    'https://via.placeholder.com/280x150/e91e63/ffffff?text=1',
-    'https://via.placeholder.com/280x150/e91e63/ffffff?text=2',
-    'https://via.placeholder.com/280x150/e91e63/ffffff?text=3',
-    'https://via.placeholder.com/280x150/e91e63/ffffff?text=4'
-  ];
-  constructor() { }
+    listImage = [
+        'https://via.placeholder.com/1280x420/e91e63/ffffff?text=1',
+        'https://via.placeholder.com/1280x420/e91e63/ffffff?text=2',
+        'https://via.placeholder.com/1280x420/e91e63/ffffff?text=3',
+        'https://via.placeholder.com/1280x420/e91e63/ffffff?text=4'
+    ];
+    itemWidth: number;
+    config = 2;
+    
+    constructor(@Inject(GalleryConfig) @Optional() config: number) {
+        if (config) {
+            this.config = config;
+        }
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.itemWidth = 95 / this.config;
+    }
 
 }
---
+++ src/app/image-gallery/token.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by diepzdeptraivodich on 17/12/2018.
+ */
+import {InjectionToken} from '@angular/core';
+export const GalleryConfig = new InjectionToken<number>('GalleryConfig');
